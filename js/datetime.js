1d9a2a819eba3ca41102ff466be0ff55175e4b1a
nesaranaeem <hello@nesaran.com>
2022-08-16 13:41:02 +0600

    simple dashboard updated

--- js/datetime.js
+++
@@ -1,8 +0,0 @@
-const getDateTime = document.getElementById("date-time");
-const today = new Date();
-const date =
-  today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-const time =
-  today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-const dateTime = date + " " + time;
-getDateTime.innerText = dateTime;
