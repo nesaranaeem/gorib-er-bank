fcb86a32247500af8aefcb53c2dd8835df641c3a
nesaranaeem <hello@nesaran.com>
2022-08-16 16:17:42 +0600

    basic validation added

--- js/diposit.js
+++ js/diposit.js
@@ -2,13 +2,18 @@ document.getElementById("btn-diposit").addEventListener("click", function () {
   //get input from input field
   const getDiposit = document.getElementById("diposit-amount");
   const newDeposit = parseFloat(getDiposit.value);
+  if (newDeposit < 1 || isNaN(newDeposit) || typeof newDeposit !== "number") {
+    alert("Sorry! Minimum Diposit is 1 Taka");
+    return;
+  }
 
   //get diposit total
   const dipositTotal = document.getElementById("diposit-total");
   // const allTimeDiposit = parseFloat(dipositTotal.innerText);
   //set diposit only bellow 50k
   if (newDeposit > 50000) {
-    alert("Sorry You Can't Deposit More Than 50000 ");
+    alert("This Is Goriber Bank! So, Sorry You Can't Deposit More Than 50000");
+    return;
   } else {
     dipositTotal.innerText = parseFloat(dipositTotal.innerText) + newDeposit;
   }
--- js/withdraw.js
+++ js/withdraw.js
@@ -2,17 +2,27 @@ document.getElementById("btn-withdraw").addEventListener("click", function () {
   //get withdraw amount
   const getWithdrawAmount = document.getElementById("withdraw-amount");
   const previousWithdrawAmount = parseFloat(getWithdrawAmount.value);
+
+  //set new total
+  const setNewTotal = document.getElementById("balance-total");
+  const newTotal = parseFloat(setNewTotal.innerText);
+  if (previousWithdrawAmount > newTotal) {
+    alert("Sorry your withdraw amount is higher than your balance");
+    return;
+  } else if (
+    isNaN(previousWithdrawAmount) ||
+    typeof newDeposit !== "previousWithdrawAmount"
+  ) {
+    alert("Sorry value can't be empty");
+    return;
+  }
   //set to withdraw
   const getWithdrawTotal = document.getElementById("withdraw-total");
   const withdrawTotal = parseFloat(getWithdrawTotal.innerText);
   const totalWithdraw = previousWithdrawAmount + withdrawTotal;
   getWithdrawTotal.innerText = totalWithdraw;
-  //set new total
-  const setNewTotal = document.getElementById("balance-total");
-  const newTotal = parseFloat(setNewTotal.innerText);
   const finalNewTotal = newTotal - previousWithdrawAmount;
   setNewTotal.innerText = finalNewTotal;
-  // if (previousWithdrawAmount > finalNewTotal) {
-  //   alert("Sorry your withdraw amount is higher than your balance ");
-  // }
+  // clear withdraw field after input
+  getWithdrawAmount.value = "";
 });
